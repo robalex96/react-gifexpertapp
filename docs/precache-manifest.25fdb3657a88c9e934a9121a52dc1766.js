2c53ae898f6d57327034131d1b63fd0e7bce893c
Robert <robert.limas@uptc.edu.co>
2020-07-26 17:53:52 -0500

    Docs agregago

---
+++ docs/precache-manifest.25fdb3657a88c9e934a9121a52dc1766.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "011318d6db4eab3d214e3ca3703ffea1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf6f5d17aed1bfc66c97",
+    "url": "/static/css/main.06579ff2.chunk.css"
+  },
+  {
+    "revision": "100caeaeffd01e7cd693",
+    "url": "/static/js/2.78557d1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.78557d1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf6f5d17aed1bfc66c97",
+    "url": "/static/js/main.0980a64f.chunk.js"
+  },
+  {
+    "revision": "e5ee6826b74226f47af5",
+    "url": "/static/js/runtime-main.b577d66e.js"
+  }
+]);
